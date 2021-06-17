39df1219aa9586f76a1e4962d7f00fb12e926f93
DiegoTGJ <DiegoTGJobs@gmail.com>
2021-06-16 21:17:23 -0400

    Fixed spanish service name

--- dgt-di/src/main/java/diego/spring/di/services/I18nSpanishGreetingService.java
+++ dgt-di/src/main/java/diego/spring/di/services/I18nSpanishGreetingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Profile("ES")
 @Service("i18nService")
-public class I18nSpanishService implements GreetingService {
+public class I18nSpanishGreetingService implements GreetingService {
 
 	@Override
 	public String sayGreeting() {
